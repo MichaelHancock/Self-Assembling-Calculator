0a0255136de95b041f2cb116fffbc2803d741836
Michael Hancock <m.hancock95@hotmail.co.uk>
2017-03-05 13:02:23 +0000

    Initial commit of working random brute force method

---
+++ Self Assembling Calculator/DanaVariable.h
@@ -0,0 +1,25 @@
+#pragma once
+
+/* --- Class to store type and name information for a Dana variable --- */
+
+class DanaVariable {
+public:
+	std::string name;
+	std::string type;
+	int size;
+
+	DanaVariable(std::string variableName, std::string variableType) {
+		name = variableName;
+		type = variableType;
+		size = NULL;
+	}
+
+	DanaVariable(std::string variableName, std::string variableType, int variableSize) {
+		name = variableName;
+		type = variableType;
+
+		if (variableSize > 0)
+			size = variableSize;
+	}
+};
+
