48c91150d6be96e8b51fcce1fb85e949ae128b7d
Michael Hancock <michael.hancock@protonmail.com>
2017-04-04 10:36:50 +0100

    Remove code not required for this version

--- Self Assembling Calculator/DanaGrammer.h
+++ Self Assembling Calculator/DanaGrammer.h
@@ -1,37 +1,8 @@
 #pragma once
-
-#include <ctime>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <sstream>
-
-#include "DanaVariable.h"
-
-typedef std::pair<std::string, std::string> pairOfStrings;
-typedef std::vector<pairOfStrings> vectorOfPairs;
-
 class DanaGrammer
 {
 public:
-	//Constructors
-	DanaGrammer(std::string, std::vector<DanaVariable>);
-
-	//Public member functions
-	std::string nextLine(int, int, std::string);
-private:
-
-	//Private member functions
-	int getRandomNumber(int, int);
-	vectorOfPairs splitStringIntoPairs(std::string, const char *);
-	std::string generateType();
-	std::string generateVariableName();
-	std::string generateFunctionCall();
-	std::string getMatchingParam(std::string);
-	pairOfStrings assignFromArray();
-
-	//Private member variables
-	vectorOfPairs availableFunctions;
-	std::vector<DanaVariable> availableVariables;
+	DanaGrammer();
+	~DanaGrammer();
 };
 
