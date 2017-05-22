2b30673d1a13908b2526bcc6639273716cb1838a
Michael Hancock <m.hancock95@hotmail.co.uk>
2017-05-22 16:43:57 +0100

    Generated all instances of primary line
    The primary (non-hardcoded) line of the function, is generated iteratively. Covering all instances

--- Self Assembling Calculator/DanaGrammer.cpp
+++ Self Assembling Calculator/DanaGrammer.cpp
@@ -10,7 +10,7 @@ DanaGrammer::DanaGrammer(std::string required, ListOfVariables variables) {
 	//	Add passed variables into local list of known variables
 	knownVariables = variables;
 
-	//	Add functions  into local list of known functions
+	//	Add functions into a local list of known functions
 	knownFunctions.push_back(DanaFunction("uInt.intFromString", "int", { "char" }));
 	knownFunctions.push_back(DanaFunction("adder.add", "int", { "int", "int" }));
 
@@ -24,26 +24,114 @@ DanaGrammer::DanaGrammer(std::string required, ListOfVariables variables) {
 	//	Add hardcoded function elements
 	functionHeader.push_back("component provides App requires" + required + " {");
 	functionHeader.push_back("\tint App:main(AppParam params[]) {");
-	functionHeader.push_back("\t\tchar a[] = params[0].string");
+	store.addNewListOfLines({ "char a[] = params[0].string" });
 }
 
 ListOfStrings DanaGrammer::generateAllFunctonInstances(int numberOfLines) {
 	ListOfStrings lines = functionHeader;
+	ListOfStrings potentialLines = {};
+	int currentLineNumber = 2;
 
 	//	Return only hardcoded elements if less than 2 lines
 	if (numberOfLines < 2) {
+		lines.push_back("\t\treturn 0");
 		lines.push_back("\t}");
 		lines.push_back("}");
 
-		return { collpaseVector(lines) };
+		return { { collpaseVector(lines) } };
 	}
 
 	//	Iterate through available functions
 	for (int i = 0; i < knownFunctions.size(); i++) {
+		DanaFunction currentFunction = knownFunctions.at(i);
+		ListOfVariables matchedParams = {};
+		
+		//	Find variables which match the currentFunction parameters  
+		for (int j = 0; j < currentFunction.numberOfParameters(); j++) {
+			const std::string currentParam = currentFunction.getParameter(j);
 
+			for (int k = 0; k < knownVariables.size(); k++) {
+				DanaVariable currentVariable = knownVariables.at(k);
+
+				if (!currentFunction.isArrayAssign && currentVariable.type == currentParam) {
+					//	If currentVariable matches one of the currentFunction parameters, add it to matched params
+					if (!(std::find(matchedParams.begin(), matchedParams.end(),
+						currentVariable) != matchedParams.end())) {
+						matchedParams.push_back(currentVariable);
+					}
+				}
+				else if (currentFunction.isArrayAssign && currentVariable.size > 0) {
+					//	If current function is an array assign. Add all valid array indexes to matched params
+					for (int l = 0; l < currentVariable.size; l++) {
+						DanaVariable a = DanaVariable(currentVariable.name, "int");
+						a.value = l;
+						matchedParams.push_back(a);
+					}
+				}
+			}
+		}
+
+		if (matchedParams.size() > 0) {
+			if (currentFunction.numberOfParameters() == 1) {
+				//	If variables have been found which match this functions parameter
+				for (int j = 0; j < matchedParams.size(); j++) {
+					std::string outputLine = currentFunction.returnType + " ";
+					outputLine += getVariableName(currentLineNumber);
+					outputLine += " = ";
+					if (!currentFunction.isArrayAssign)
+						outputLine += currentFunction.name + " ( " + matchedParams.at(j).name + " )";
+					else
+						outputLine += matchedParams.at(j).name + " [ " + std::to_string(matchedParams.at(j).value) + " ]";
+
+					potentialLines.push_back(outputLine);
+				}
+			}
+			else if (currentFunction.numberOfParameters() == 2 && currentFunction.getParameter(0) == currentFunction.getParameter(1)) {
+				//	If variables have been found which match one of the two parameters of this function
+				const std::string currentParam = currentFunction.getParameter(0);
+				ListOfVariables filteredParams = matchedParams;
+
+				//	Filter variables which cannot be used to populate this functions parameters
+				filteredParams.erase(std::remove_if(filteredParams.begin(), filteredParams.end(),
+					[currentParam](DanaVariable v) { return v.type != currentParam; }), filteredParams.end());
+
+				//	Generate all pair combinations of variables for this function
+				for (int j = 0; j < filteredParams.size(); j++) {
+					for (int k = 0; k < filteredParams.size(); k++) {
+						std::string outputLine = currentFunction.returnType + " ";
+						outputLine += getVariableName(currentLineNumber);
+						outputLine += " = ";
+						outputLine += currentFunction.name + " ( " + filteredParams.at(j).name + ", " + filteredParams.at(k).name + " )";
+						potentialLines.push_back(outputLine);
+					}
+				}
+			}
+		}
+	}
+
+	//	Add generated lines to store and output generated functions
+	store.addNewListOfLines(potentialLines);
+	std::string variableToReturn = getVariableName(currentLineNumber);
+	currentLineNumber++;
+	for (int i = 0; i < store.getList(0).size(); i++) {
+		for (int j = 0; j < store.getList(1).size(); j++) {
+			ListOfStrings output = functionHeader;
+			output.push_back("\t\t" + store.getList(0).at(i));
+			output.push_back("\t\t" + store.getList(1).at(j));
+			output.push_back("\t\treturn " + variableToReturn);
+			output.push_back("\t}");
+			output.push_back("}");
+
+			generatedFunctions.push_back(collpaseVector(output));
+		}
 	}
 
 	return generatedFunctions;
+}
+
+std::string DanaGrammer::getVariableName(int lineNumber) {
+	assert(lineNumber >= 1 && lineNumber <= 26);
+		return std::string(1, "abcdefghijklmnopqrstuvwxyz"[lineNumber - 1]);
 }
 
 std::string DanaGrammer::collpaseVector(ListOfStrings lines) {
--- Self Assembling Calculator/DanaGrammer.h
+++ Self Assembling Calculator/DanaGrammer.h
@@ -1,7 +1,11 @@
 #pragma once
 #include <vector>
+#include <assert.h>   
+#include <string>
+#include <algorithm>
 #include "DanaVariable.h"
 #include "DanaFunction.h"
+#include "LineStore.h"
 
 typedef std::vector<DanaFunction> ListOfFunctions;
 typedef std::vector<DanaVariable> ListOfVariables;
@@ -13,7 +17,9 @@ private:
 	ListOfVariables knownVariables;
 	ListOfStrings generatedFunctions;
 	ListOfStrings functionHeader;
+	LineStore store;
 	std::string collpaseVector(ListOfStrings lines);
+	std::string getVariableName(int lineNumber);
 
 public:
 	DanaGrammer(std::string required, ListOfVariables knownVariables);
